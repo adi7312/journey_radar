b66be3ac1338164182088e566f6e6655eb950fe3
adi7312 <zdrian.alewski@gmail.com>
2025-10-05 05:41:51 +0000

    i didnt sleep in 24h

--- api_gw/trip/trip_controller.py
+++ api_gw/trip/trip_controller.py
@@ -52,7 +52,7 @@ def get_trip_geo(request: TripRequest) -> TripResponse:
     else:
         delay = rsp.json()['delay_seconds']
     
-    p_delay = call_predict(formatted,e["line_name"], e['vehicle_type'], dep_lat, dep_lng)['delay_sec']
+    p_delay = call_predict(formatted,e["line_name"], e['vehicle_type'], dep_lat, dep_lng).json()["delay_sec"]
 
     return TripResponse(
         distance_m=google_rsp.json()["route"]["distance_m"],
